02bce0a4be8bccb31b16ececf6ee40ebf0e043da
Sofia Mara <62350286+Sofia331@users.noreply.github.com>
2022-03-08 18:32:38 -0300

    >.<

--- Retições/Exercicio01/Exercicio01.js
+++ Retições/Exercicio01/Exercicio01.js
@@ -1,13 +1,16 @@
 function contar() {
-    let inicio= window.getElementById('inicio')
-    let fim = window.getElementById('fim')
-    let pular = window.getElementById('pular')
-    
-    if(inicio.value.length == 0 || fim.value.length == 0 || pular.value.length == 0 ){
-        window.alert("[ERRO]Faltam dados!! Verifique novamente.")
-    }else{
-        window.alert("Esta certinho >.<")
-    }
+    let inicio= window.getElementById('txtI')
+    let fim = window.getElementById('txtF')
+    let pular = window.getElementById('txtP')
 
+    window.alert('[ERRO]Faltam dados!! Verifique novamente.')
+    
+    /*
+    if(inicio.value.length == 0 || fim.value.length == 0 || pular.value.length == 0 ){
+        window.alert('[ERRO]Faltam dados!! Verifique novamente.')
+    }else{
+        window.alert('Esta certinho >.<')
+    }
+    */
 
 }
