62a7b6a6b648894566c67c5b408ef8cf3df7ad7a
Sofia Mara <62350286+Sofia331@users.noreply.github.com>
2022-03-08 21:11:37 -0300

    >.<

--- Retições/Exercicio01/Exercicio01.js
+++ Retições/Exercicio01/Exercicio01.js
@@ -1,16 +1,36 @@
 function contar() {
-    let inicio= window.getElementById('txtI')
-    let fim = window.getElementById('txtF')
-    let pular = window.getElementById('txtP')
-
-    window.alert('[ERRO]Faltam dados!! Verifique novamente.')
+    let inicio = document.getElementById('txtI')
+    let fim = document.getElementById('txtF')
+    let pular = document.getElementById('txtP')
+    let res = document.getElementById('res')
     
-    /*
+    
     if(inicio.value.length == 0 || fim.value.length == 0 || pular.value.length == 0 ){
-        window.alert('[ERRO]Faltam dados!! Verifique novamente.')
+        res.innerHTML = 'Faltam dados!! Verifique novamente.'
+        //window.alert('[ERRO]Faltam dados!! Verifique novamente.')
     }else{
-        window.alert('Esta certinho >.<')
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(pular.value)
+        if(p <= 0){
+            window.alert('Passo Invalido! Irei considerar Passo = 1')
+            p = 1
+        }
+        if(i < f){
+            for(let c = i; c <= f; c +=p){
+                res.innerHTML += ` ${c}`} 
+            }
+            else 
+            {
+                for(let c = i; c >= f; c -=p)
+                {
+                    res.innerHTML += `${c}`
+                }
+            }
+            res.innerHTML += `\u{1F3C1}`
+        
     }
-    */
+    
 
 }
